de7f8040f9d3ea3d963d489470dbfee359b8f85f
ashu <aashuk012@gmail.com>
2021-02-26 17:46:19 +0530

    dbchnage

--- satabazer/settings.py
+++ satabazer/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'satabazer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogdb',
+        'USER': 'blog_admin',
+        'PASSWORD': 'testing123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
