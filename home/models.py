a74a71ee1911b2d7af33531a842733d55796d47b
ashu <aashuk012@gmail.com>
2021-03-05 23:34:50 +0530

    llll

--- home/models.py
+++ home/models.py
@@ -16,7 +16,6 @@ class Post(models.Model):
 class Rahul(models.Model):
     name = models.CharField(max_length=200)
     time = models.CharField(max_length=200)
-    num =  models.TextField()
     stor = models.TextField()   
 
     def __str__(self):
