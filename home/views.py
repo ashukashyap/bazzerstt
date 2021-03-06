f6056f7284593f4462ca58eda6664e1a19b29bf5
ashu <aashuk012@gmail.com>
2021-03-06 11:19:08 +0530

    yaag

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post,Rahul,Table,Contact,Singup,Record,blog
+from .models import Post,Table,Contact,Singup,Record,blog
 from django.contrib import messages
 
 
